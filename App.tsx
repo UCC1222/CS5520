f4553a909b171bb47807fe9eb641bae31627e5bb
Sisi You <you.si@northeastern.edu>
2025-01-06 14:46:32 -0800

    edit App.tsx and tsconfig.json

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to CC's APP!</Text>
+      <Button title="Click Me" onPress={() => alert('Button Pressed!')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
   },
 });
