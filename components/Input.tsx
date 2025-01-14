bb0d9ee1b10f324d3949e5845b8069cb6af75ea2
Sisi You <you.si@northeastern.edu>
2025-01-14 00:38:04 -0800

    Add autoFocus prop to Input component for conditional focus

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 
-export default function Input() {
+export default function Input({ autoFocus }: { autoFocus: boolean }) {
+  const inputRef = useRef<TextInput>(null);
+
+  useEffect(() => {
+    if (autoFocus && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [autoFocus]);
+
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} placeholder="Type something..." />
+      <TextInput
+        ref={inputRef}
+        style={styles.input}
+        placeholder="Type something..."
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    width: '100%',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-  },
-});
