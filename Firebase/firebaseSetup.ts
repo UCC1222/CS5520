c9558fefbf8e94ac3c287114f479f3c35d0a8b24
Sisi You <you.si@northeastern.edu>
2025-02-03 15:17:24 -0800

    Configured and initialized Firebase and Firestore, exporting the database object for global usage.

---
+++ Firebase/firebaseSetup.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnjqD2YD3gVV_t6nIfnQ6lIsWgte3u64s",
+  authDomain: "cc5520.firebaseapp.com",
+  projectId: "cc5520",
+  storageBucket: "cc5520.firebasestorage.app",
+  messagingSenderId: "909431427445",
+  appId: "1:909431427445:web:a8b96345a80c2184ed433c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
