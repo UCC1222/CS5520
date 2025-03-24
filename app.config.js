ef1fe86be0051f2a4a8cb4bed7f2aa9956e5f886
Sisi You <you.si@northeastern.edu>
2025-03-24 15:02:55 -0700

    Add app.config.js with Expo configuration

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default {
+    expo: {
+      name: "MyApp",
+      slug: "my-app",
+      version: "1.0.0",
+      extra: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+  };
